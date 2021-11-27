44a44faee37ab58d45f901e8230402cf1a83c6bb
chrism <christoper.and.moran@gmail.com>
2021-11-26 22:14:49 -0500

    Adds basic CLI for starting Host

--- src/bin/start_host.rs
+++ src/bin/start_host.rs
@@ -1,18 +1,52 @@
 use rhiza::host::{Host, HostConfig};
-use tokio::time::{sleep, Duration};
+// use tokio::time::{sleep, Duration};
+use tokio::signal;
+
+use clap::{App, Arg};
 
 #[tokio::main]
 async fn main() {
-    let cfg = HostConfig::new("wlp3s0")
-        .socket_num(25_000)
-        .store_filename("store");
+    let matches = App::new("Rhiza Host")
+        .version("0.1")
+        .author("Christopher Moran <christopher.and.moran@gmail.com>")
+        .about("Start a Rhiza host")
+        .arg(
+            Arg::new("interface")
+                .short('i')
+                .long("interface")
+                .default_value("lo")
+                .about("Sets the proper network interface"),
+        )
+        .arg(
+            Arg::new("socket")
+                .short('s')
+                .long("tcp_socket_num")
+                .default_value("25000")
+                .about("Sets an alternative TCP socket"),
+        )
+        .arg(
+            Arg::new("store_filename")
+                .short('f')
+                .long("store_filename")
+                .default_value("store")
+                .about("Sets the filename for the `sled`-based key-value store"),
+        )
+        .get_matches();
+
+    let interface: String = matches.value_of("interface").unwrap().to_string();
+    let store_filename: String = matches.value_of("store_filename").unwrap().to_string();
+    let socket: usize = matches.value_of("socket").unwrap().parse().unwrap();
+
+    let cfg = HostConfig::new(interface)
+        .socket_num(socket)
+        .store_filename(store_filename);
     let mut host = Host::from_config(cfg).unwrap();
     host.start().await.unwrap();
 
-    println!("Host should be running in the background");
+    println!("Rhiza Host should be running in the background");
     // Other tasks can operate while the host is running on it's own thread
-    sleep(Duration::from_secs(10)).await;
+    signal::ctrl_c().await.unwrap();
+    println!("\nShutting down Rhiza Host");
 
-    // Need to kill host manually
     host.stop().await.unwrap();
 }
