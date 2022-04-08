4a61781b03a1aadeff73f52c90182bb1ac10ff90
chrism <christopher.and.moran@gmail.com>
2022-04-08 13:52:56 -0400

    Remove CLI example, fix CI

--- tools/ci/src/main.rs
+++ tools/ci/src/main.rs
@@ -25,7 +25,7 @@ fn main() {
     let args: Vec<String> = std::env::args().collect();
     if args.get(1) != Some(&"nonlocal".to_string()) {
         // Run tests
-        cmd!("cargo test --workspace -- --test-threads=1")
+        cmd!("cargo test --workspace -- --nocapture --test-threads=1")
             .run()
             .expect("Please fix failing tests in output above.");
 
