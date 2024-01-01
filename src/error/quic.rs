0cdf0789eb517bd8975a5db668ef4f55683e2a19
chrism <cmoran@cmoran.xyz>
2024-01-01 18:54:02 -0500

    Do the same with QUIC errors

--- src/error/quic.rs
+++ src/error/quic.rs
@@ -1,69 +1,38 @@
 use core::fmt::{Display, Formatter};
 use serde::*;
 
+use thiserror::Error;
+
 #[cfg(feature = "quic")]
-#[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
+#[derive(Clone, Debug, Error, Eq, PartialEq, Serialize, Deserialize)]
 pub enum Quic {
+    #[error("Generic Quic Issue")]
     QuicIssue,
+    #[error("Error with deserialization into proper GenericMsg")]
     BadGenericMsg,
+    #[error("Error opening bidirectional stream from connection")]
     OpenBi,
+    #[error("Error reading bytes from stream receiver")]
     RecvRead,
+    #[error("Error acquiring owned connection")]
     Connection,
     // Error accessing an owned Endpoint
+    #[error("Unable to access owned Endpoint")]
     AccessEndpoint,
+    #[error("Unable to establish Connection to remote Endpoint")]
     EndpointConnect,
+    #[error("Unable to find .pem key file")]
     FindKeys,
+    #[error("Error reading .pem key file")]
     ReadKeys,
+    #[error("Unable to find .pem certificates")]
     FindCerts,
+    #[error("Error reading .pem certificates")]
     ReadCerts,
+    #[error("Error configuring server with certificate")]
     Configuration,
+    #[error("Error creating endpoint from configuration parameters")]
     EndpointCreation,
-}
-
-#[cfg(feature = "quic")]
-impl std::error::Error for Quic {
-    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-        use Quic::*;
-        match *self {
-            QuicIssue => None,
-            BadGenericMsg => None,
-            OpenBi => None,
-            RecvRead => None,
-            Connection => None,
-            // Error accessing an owned Endpoint
-            AccessEndpoint => None,
-            EndpointConnect => None,
-            FindKeys => None,
-            ReadKeys => None,
-            FindCerts => None,
-            ReadCerts => None,
-            Configuration => None,
-            EndpointCreation => None,
-        }
-    }
-}
-
-#[cfg(feature = "quic")]
-impl Display for Quic {
-    fn fmt(&self, f: &mut Formatter) -> core::fmt::Result {
-        use Quic::*;
-        match self {
-            QuicIssue => write!(f, "Generic Quic Issue"),
-            BadGenericMsg => write!(f, "Error with deserialization into proper GenericMsg"),
-            OpenBi => write!(f, "Error opening bidirectional stream from connection"),
-            RecvRead => write!(f, "Error reading bytes from stream receiver"),
-            Connection => write!(f, "Error acquiring owned connection"),
-            // Error accessing an owned Endpoint
-            AccessEndpoint => write!(f, "Unable to access owned Endpoint"),
-            EndpointConnect => write!(f, "Unable to establish Connection to remote Endpoint"),
-            FindKeys => write!(f, "Unable to find .pem key file"),
-            ReadKeys => write!(f, "Error reading .pem key file"),
-            FindCerts => write!(f, "Unable to find .pem certificates"),
-            ReadCerts => write!(f, "Error reading .pem certificates"),
-            Configuration => write!(f, "Error configuring server with certificate"),
-            &EndpointCreation => write!(f, "Error creating endpoint from configuration parameters"),
-        }
-    }
 }
 
 #[cfg(feature = "quic")]
