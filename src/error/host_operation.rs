8b5842a3e644c813cb5d6e0fdfce8558e6076367
chrism <cmoran@cmoran.xyz>
2024-01-01 18:55:40 -0500

    HostOperation subset

--- src/error/host_operation.rs
+++ src/error/host_operation.rs
@@ -3,31 +3,12 @@ use serde::*;
 
 use thiserror::Error;
 
-#[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
+#[derive(Clone, Debug, Error, Eq, PartialEq, Serialize, Deserialize)]
 pub enum HostOperation {
+    #[error("Unsuccessful Host-side SET operation")]
     SetFailure,
+    #[error("Unsuccessful Host-side SET operation")]
     GetFailure,
+    #[error("Unsuccessful Host connection")]
     ConnectionError,
-}
-
-impl std::error::Error for HostOperation {
-    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-        use HostOperation::*;
-        match *self {
-            SetFailure => None,
-            GetFailure => None,
-            ConnectionError => None,
-        }
-    }
-}
-
-impl Display for HostOperation {
-    fn fmt(&self, f: &mut Formatter) -> core::fmt::Result {
-        use HostOperation::*;
-        match *self {
-            SetFailure => write!(f, "Unsuccessful Host-side SET operation"),
-            GetFailure => write!(f, "Unsuccessful Host-side SET operation"),
-            ConnectionError => write!(f, "Unsuccessful Host connection"),
-        }
-    }
 }
