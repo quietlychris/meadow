b22c4790df4d3e580ad242b4477b42515a13935a
chrism <christopher.and.moran@gmail.com>
2024-01-20 19:12:22 -0500

    Cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,10 @@
 #![allow(unused_imports)]
 
 //! `meadow` is an experimental robotics-focused publish/request middleware
-//! for embedded Linux. It is built with a high preference for catching errors 
-//! at compile-time over runtime and a focus on developer ergonomics, and can 
+//! for embedded Linux. It is built with a high preference for catching errors
+//! at compile-time over runtime and a focus on developer ergonomics, and can
 //! natively operate on any [`serde`](https://serde.rs/)-compatible data type.
-//! 
+//!
 //! It uses a star-shaped network topology, with a focus
 //! on ease-of-use and transparent design and operation. It is more similar to
 //! [ZeroMQ](https://zguide.zeromq.org/docs/chapter1/) than to higher-level frameworks like [ROS/2](https://design.ros2.org/articles/discovery_and_negotiation.html),
