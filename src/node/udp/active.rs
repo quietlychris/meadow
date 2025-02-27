e754038f12f49b615f0515941e833c1dbfe796f8
chrism <christopher.and.moran@gmail.com>
2025-02-27 11:19:56 -0500

    Consolidate Node<Udp> methods

--- src/node/udp/active.rs
+++ src/node/udp/active.rs
@@ -41,10 +41,13 @@ impl<T: Message> From<Node<Nonblocking, Udp, Idle, T>> for Node<Nonblocking, Udp
     }
 }
 
-impl<T: Message + 'static> Node<Nonblocking, Udp, Active, T> {
+use crate::node::Block;
+use std::fmt::Debug;
+
+impl<T: Message + 'static, B: Block + Debug> Node<B, Udp, Active, T> {
     #[tracing::instrument]
     #[inline]
-    pub async fn publish(&self, val: T) -> Result<(), Error> {
+    async fn publish_internal(&self, val: T) -> Result<(), Error> {
         let packet = Msg::new(MsgType::Set, self.topic.clone(), val)
             .to_generic()?
             .as_bytes()?;
@@ -60,7 +63,9 @@ impl<T: Message + 'static> Node<Nonblocking, Udp, Active, T> {
         Ok(())
     }
 
-    pub async fn publish_msg(&self, msg: Msg<T>) -> Result<(), Error> {
+    #[tracing::instrument]
+    #[inline]
+    async fn publish_msg_internal(&self, msg: Msg<T>) -> Result<(), Error> {
         let packet = msg.to_generic()?.as_bytes()?;
         let socket = match self.socket.as_ref() {
             Some(socket) => socket,
@@ -75,8 +80,8 @@ impl<T: Message + 'static> Node<Nonblocking, Udp, Active, T> {
 
     #[tracing::instrument]
     #[inline]
-    pub async fn request(&self) -> Result<Msg<T>, Error> {
-        let packet = GenericMsg::get::<T>(self.topic.clone()).as_bytes()?;
+    async fn request_nth_back_internal(&self, n: usize) -> Result<Msg<T>, Error> {
+        let packet = GenericMsg::get_nth::<T>(self.topic.clone(), n).as_bytes()?;
         let buffer = self.buffer.clone();
 
         if let Some(socket) = &self.socket {
@@ -90,7 +95,7 @@ impl<T: Message + 'static> Node<Nonblocking, Udp, Active, T> {
 
     #[tracing::instrument]
     #[inline]
-    pub async fn topics(&self) -> Result<Msg<Vec<String>>, Error> {
+    async fn topics_internal(&self) -> Result<Msg<Vec<String>>, Error> {
         let packet = GenericMsg::topics().as_bytes()?;
         let buffer = self.buffer.clone();
 
@@ -101,6 +106,34 @@ impl<T: Message + 'static> Node<Nonblocking, Udp, Active, T> {
         } else {
             Err(Error::AccessSocket)
         }
+    }
+}
+
+impl<T: Message + 'static> Node<Nonblocking, Udp, Active, T> {
+    #[tracing::instrument]
+    #[inline]
+    pub async fn publish(&self, val: T) -> Result<(), Error> {
+        self.publish_internal(val).await?;
+        Ok(())
+    }
+
+    pub async fn publish_msg(&self, msg: Msg<T>) -> Result<(), Error> {
+        self.publish_msg_internal(msg).await?;
+        Ok(())
+    }
+
+    #[tracing::instrument]
+    #[inline]
+    pub async fn request(&self) -> Result<Msg<T>, Error> {
+        let msg = self.request_nth_back_internal(0).await?;
+        Ok(msg)
+    }
+
+    #[tracing::instrument]
+    #[inline]
+    pub async fn topics(&self) -> Result<Msg<Vec<String>>, Error> {
+        let msg = self.topics_internal().await?;
+        Ok(msg)
     }
 }
 
@@ -134,114 +167,60 @@ impl<T: Message + 'static> Node<Blocking, Udp, Active, T> {
     #[tracing::instrument]
     #[inline]
     pub fn publish(&self, val: T) -> Result<(), Error> {
-        let packet = Msg::new(MsgType::Set, self.topic.clone(), val)
-            .to_generic()?
-            .as_bytes()?;
-
-        let socket = match self.socket.as_ref() {
-            Some(socket) => socket,
-            None => return Err(Error::AccessSocket),
-        };
-
-        let handle = match &self.rt_handle {
-            Some(handle) => handle,
-            None => return Err(Error::HandleAccess),
-        };
-
-        handle.block_on(async {
-            socket
-                .send_to(&packet, self.cfg.network_cfg.host_addr)
-                .await?;
-            Ok(())
-        })
+        match &self.rt_handle {
+            Some(handle) => handle.block_on(async {
+                self.publish_internal(val).await?;
+                Ok(())
+            }),
+            None => Err(Error::HandleAccess),
+        }
     }
 
     #[tracing::instrument]
     #[inline]
     pub fn publish_msg(&self, msg: Msg<T>) -> Result<(), Error> {
-        let packet = msg.to_generic()?.as_bytes()?;
-
-        let socket = match self.socket.as_ref() {
-            Some(socket) => socket,
-            None => return Err(Error::AccessSocket),
-        };
-
-        let handle = match &self.rt_handle {
-            Some(handle) => handle,
-            None => return Err(Error::HandleAccess),
-        };
-
-        handle.block_on(async {
-            socket
-                .send_to(&packet, self.cfg.network_cfg.host_addr)
-                .await?;
-            Ok(())
-        })
+        match &self.rt_handle {
+            Some(handle) => handle.block_on(async {
+                self.publish_msg_internal(msg).await?;
+                Ok(())
+            }),
+            None => Err(Error::HandleAccess),
+        }
     }
 
     #[tracing::instrument]
     #[inline]
     pub fn request(&self) -> Result<Msg<T>, Error> {
-        let packet = GenericMsg::get::<T>(self.topic.clone()).as_bytes()?;
-        let buffer = self.buffer.clone();
-
-        let handle = match &self.rt_handle {
-            Some(handle) => handle,
-            None => return Err(Error::HandleAccess),
-        };
-
-        handle.block_on(async {
-            if let Some(socket) = &self.socket {
-                send_msg(socket, packet, self.cfg.network_cfg.host_addr).await?;
-                let msg = await_response(socket, buffer).await?.try_into()?;
+        match &self.rt_handle {
+            Some(handle) => handle.block_on(async {
+                let msg = self.request_nth_back_internal(0).await?;
                 Ok(msg)
-            } else {
-                Err(Error::AccessSocket)
-            }
-        })
+            }),
+            None => Err(Error::HandleAccess),
+        }
     }
 
     #[tracing::instrument]
     #[inline]
     pub fn request_nth_back(&self, n: usize) -> Result<Msg<T>, Error> {
-        let packet = GenericMsg::get_nth::<T>(self.topic.clone(), n).as_bytes()?;
-        let buffer = self.buffer.clone();
-
-        let handle = match &self.rt_handle {
-            Some(handle) => handle,
-            None => return Err(Error::HandleAccess),
-        };
-
-        handle.block_on(async {
-            if let Some(socket) = &self.socket {
-                send_msg(socket, packet, self.cfg.network_cfg.host_addr).await?;
-                let msg = await_response(socket, buffer).await?.try_into()?;
+        match &self.rt_handle {
+            Some(handle) => handle.block_on(async {
+                let msg = self.request_nth_back_internal(n).await?;
                 Ok(msg)
-            } else {
-                Err(Error::AccessSocket)
-            }
-        })
+            }),
+            None => Err(Error::HandleAccess),
+        }
     }
 
     #[tracing::instrument]
     #[inline]
     pub fn topics(&self) -> Result<Msg<Vec<String>>, Error> {
-        let packet = GenericMsg::topics().as_bytes()?;
-        let buffer = self.buffer.clone();
-
-        let handle = match &self.rt_handle {
-            Some(handle) => handle,
-            None => return Err(Error::HandleAccess),
-        };
-
-        handle.block_on(async {
-            if let Some(socket) = &self.socket {
-                send_msg(socket, packet, self.cfg.network_cfg.host_addr).await?;
-                let msg = await_response(socket, buffer).await?.try_into()?;
+        match &self.rt_handle {
+            Some(handle) => handle.block_on(async {
+                let msg = self.topics_internal().await?;
                 Ok(msg)
-            } else {
-                Err(Error::AccessSocket)
-            }
-        })
+            }),
+            None => Err(Error::HandleAccess),
+        }
     }
 }
