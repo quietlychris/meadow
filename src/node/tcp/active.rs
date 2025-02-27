2810047937cf241bbeacf9c9883a6f0978049404
chrism <christopher.and.moran@gmail.com>
2025-02-27 11:06:47 -0500

    Finish consolidating TCP methods

--- src/node/tcp/active.rs
+++ src/node/tcp/active.rs
@@ -74,7 +74,7 @@ impl<T: Message + 'static, B: Block + Debug> Node<B, Tcp, Active, T> {
 
     #[tracing::instrument]
     #[inline]
-    pub async fn publish_msg_internal(&self, msg: Msg<T>) -> Result<(), Error> {
+    async fn publish_msg_internal(&self, msg: Msg<T>) -> Result<(), Error> {
         let packet = msg.to_generic()?.as_bytes()?;
         let stream = match self.stream.as_ref() {
             Some(stream) => stream,
@@ -120,6 +120,39 @@ impl<T: Message + 'static, B: Block + Debug> Node<B, Tcp, Active, T> {
         }
         Ok(())
     }
+
+    /// Request data from host on Node's assigned topic
+    #[tracing::instrument]
+    #[inline]
+    async fn request_nth_back_internal(&self, n: usize) -> Result<Msg<T>, Error> {
+        let stream = match self.stream.as_ref() {
+            Some(stream) => stream,
+            None => return Err(Error::AccessStream),
+        };
+
+        let packet = GenericMsg::get_nth::<T>(self.topic.clone(), n).as_bytes()?;
+
+        let mut buffer = self.buffer.lock().await;
+        send_msg(stream, packet).await?;
+        let msg = await_response(stream, &mut buffer).await?.try_into()?;
+        Ok(msg)
+    }
+
+    #[tracing::instrument]
+    #[inline]
+    async fn topics_internal(&self) -> Result<Msg<Vec<String>>, Error> {
+        let stream = match self.stream.as_ref() {
+            Some(stream) => stream,
+            None => return Err(Error::AccessStream),
+        };
+
+        let packet = GenericMsg::topics().as_bytes()?;
+
+        let mut buffer = self.buffer.lock().await;
+        send_msg(stream, packet).await?;
+        let msg = await_response(stream, &mut buffer).await?.try_into()?;
+        Ok(msg)
+    }
 }
 
 impl<T: Message + 'static> Node<Nonblocking, Tcp, Active, T> {
@@ -143,32 +176,22 @@ impl<T: Message + 'static> Node<Nonblocking, Tcp, Active, T> {
     #[tracing::instrument]
     #[inline]
     pub async fn request(&self) -> Result<Msg<T>, Error> {
-        let stream = match self.stream.as_ref() {
-            Some(stream) => stream,
-            None => return Err(Error::AccessStream),
-        };
+        let msg = self.request_nth_back_internal(0).await?;
+        Ok(msg)
+    }
 
-        let packet = GenericMsg::get::<T>(self.topic.clone()).as_bytes()?;
-
-        let mut buffer = self.buffer.lock().await;
-        send_msg(stream, packet).await?;
-        let msg = await_response(stream, &mut buffer).await?.try_into()?;
+    /// Request n'th data from host on Node's assigned topic
+    #[tracing::instrument]
+    #[inline]
+    pub async fn request_nth_back(&self, n: usize) -> Result<Msg<T>, Error> {
+        let msg = self.request_nth_back_internal(n).await?;
         Ok(msg)
     }
 
     #[tracing::instrument]
     #[inline]
     pub async fn topics(&self) -> Result<Msg<Vec<String>>, Error> {
-        let stream = match self.stream.as_ref() {
-            Some(stream) => stream,
-            None => return Err(Error::AccessStream),
-        };
-
-        let packet = GenericMsg::topics().as_bytes()?;
-
-        let mut buffer = self.buffer.lock().await;
-        send_msg(stream, packet).await?;
-        let msg = await_response(stream, &mut buffer).await?.try_into()?;
+        let msg = self.topics_internal().await?;
         Ok(msg)
     }
 }
@@ -206,70 +229,37 @@ impl<T: Message + 'static> Node<Blocking, Tcp, Active, T> {
     #[tracing::instrument]
     #[inline]
     pub fn request(&self) -> Result<Msg<T>, Error> {
-        let stream = match self.stream.as_ref() {
-            Some(stream) => stream,
-            None => return Err(Error::AccessStream),
-        };
-
-        let packet = GenericMsg::get::<T>(&self.topic).as_bytes()?;
-
-        let handle = match &self.rt_handle {
-            Some(handle) => handle,
-            None => return Err(Error::HandleAccess),
-        };
-
-        handle.block_on(async {
-            let mut buffer = self.buffer.lock().await;
-            send_msg(stream, packet).await?;
-            let msg = await_response(stream, &mut buffer).await?.try_into()?;
-            Ok(msg)
-        })
+        match &self.rt_handle {
+            Some(handle) => handle.block_on(async {
+                let msg = self.request_nth_back_internal(0).await?;
+                Ok(msg)
+            }),
+            None => Err(Error::HandleAccess),
+        }
     }
 
     /// Request data from host on Node's assigned topic
     #[tracing::instrument]
     #[inline]
     pub fn request_nth_back(&self, n: usize) -> Result<Msg<T>, Error> {
-        let stream = match self.stream.as_ref() {
-            Some(stream) => stream,
-            None => return Err(Error::AccessStream),
-        };
-
-        let packet = GenericMsg::get_nth::<T>(&self.topic, n).as_bytes()?;
-
-        let handle = match &self.rt_handle {
-            Some(handle) => handle,
-            None => return Err(Error::HandleAccess),
-        };
-
-        handle.block_on(async {
-            let mut buffer = self.buffer.lock().await;
-            send_msg(stream, packet).await?;
-            let msg = await_response(stream, &mut buffer).await?.try_into()?;
-            Ok(msg)
-        })
+        match &self.rt_handle {
+            Some(handle) => handle.block_on(async {
+                let msg = self.request_nth_back_internal(n).await?;
+                Ok(msg)
+            }),
+            None => Err(Error::HandleAccess),
+        }
     }
 
     #[tracing::instrument]
     #[inline]
     pub fn topics(&self) -> Result<Msg<Vec<String>>, Error> {
-        let stream = match self.stream.as_ref() {
-            Some(stream) => stream,
-            None => return Err(Error::AccessStream),
-        };
-
-        let packet = GenericMsg::topics().as_bytes()?;
-
-        if let Some(handle) = &self.rt_handle {
-            handle.block_on(async {
-                let mut buffer = self.buffer.lock().await;
-                send_msg(stream, packet).await?;
-                let msg: Msg<Vec<String>> =
-                    await_response(stream, &mut buffer).await?.try_into()?;
+        match &self.rt_handle {
+            Some(handle) => handle.block_on(async {
+                let msg = self.topics_internal().await?;
                 Ok(msg)
-            })
-        } else {
-            Err(Error::HandleAccess)
+            }),
+            None => Err(Error::HandleAccess),
         }
     }
 }
