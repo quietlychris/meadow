21e2ff374d47dc7d8bda101e9c3eddf16b1d1d84
chrism <cmoran@cmoran.xyz>
2025-07-06 16:50:06 -0400

    Finish Store implementations

--- src/host/redb.rs
+++ src/host/redb.rs
@@ -1,8 +1,9 @@
 use redb::{Database, ReadableTable, TableDefinition};
+ use redb::TableHandle;
 
 use crate::host::GenericStore;
 use crate::msg::GenericMsg;
-use crate::msg::{Message, Msg};
+use crate::msg::*;
 use crate::Error;
 use std::convert::TryInto;
 use std::sync::Arc;
@@ -38,7 +39,6 @@ impl GenericStore for Arc<Database> {
         let definition = TableDefinition::<&[u8], &[u8]>::new(&topic);
         let read_txn = self.begin_read().unwrap();
 
-        let table = read_txn.open_table(definition).unwrap();
         if let Ok(table) = read_txn.open_table(definition) {
             if let Ok((_k, v)) = table.iter().unwrap().nth_back(1).unwrap() {
                 //println!("nth_back: {:?}, {:?}",k.value(),v.value());
@@ -56,6 +56,16 @@ impl GenericStore for Arc<Database> {
 use crate::host::Store;
 
 impl Store for Arc<Database> {
+
+    fn get<T: Message>(
+        &self,
+        topic: impl Into<String> + std::fmt::Debug,
+    ) -> Result<Msg<T>, crate::Error> {
+        let generic = self.get_generic_nth(topic.into(), 0)?;
+        let msg: Msg<T> = generic.try_into()?;
+        Ok(msg)
+    }
+
     #[inline]
     fn get_nth_back<T: Message>(
         &self,
@@ -65,5 +75,38 @@ impl Store for Arc<Database> {
         let generic = self.get_generic_nth(topic.into(), n)?;
         let msg: Msg<T> = generic.try_into()?;
         Ok(msg)
+    }
+
+    #[inline]
+    fn insert_msg<T: Message>(&mut self, msg: Msg<T>) -> Result<(), crate::Error> {
+        let generic: GenericMsg = msg.try_into()?;
+        self.insert_generic(generic)?;
+
+        Ok(())
+    }
+
+    fn insert<T: Message>(
+        &mut self,
+        topic: impl Into<String> + std::fmt::Debug,
+        data: T,
+    ) -> Result<(), crate::Error> {
+        let msg = Msg::new(MsgType::Set, topic, data);
+        self.insert_msg(msg)?;
+        Ok(())
+    }
+
+    fn topics(&self) -> Result<Vec<String>, crate::Error> {
+            // Check that the `Database` has the table (and only the table) that we created
+        let read_txn = self.begin_read().unwrap();
+        let tables = read_txn.list_tables().unwrap();
+        let mut topics = Vec::new();
+        
+        for table in tables {
+            topics.push(table.name().to_string());
+            // println!("Table: {}", table.name());
+            // let _d = TableDefinition::<&str, u32>::new(table.name());
+        }
+        topics.sort();
+        Ok(topics)
     }
 }
